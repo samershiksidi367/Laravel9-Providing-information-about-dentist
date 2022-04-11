685eaf487285726b8d01c0d9602fe2062a5fb587
samershiksidi367 <samershiksidi367@gmail.com>
2022-04-11 17:06:56 +0300

    enter

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,5 @@
 <?php
-
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -12,10 +12,20 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::get('/hello', function () {
+    return 'Hello World';
+});
 
-Route::get('/', function () {
+
+Route::get('/welcome', function () {
     return view('welcome');
 });
+//3- call 
+//Route::get(Url:'/test',[HomeController::class,test,'index'])->name(name:'test');
+//Route::get(Url:'/',[\App\Http\Controllers\HomeController::class,'index'])->name(name:'home');
+//4- Route-> co
+//5  route with parameters
+//Route::get(Url:'/param/{id}',[HomeComtroller::class,'param'])->name(name:'param');
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
